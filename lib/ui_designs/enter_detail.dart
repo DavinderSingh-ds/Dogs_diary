06b26a0e5ef728e887d335854f0dc081ad45bff2
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-02-27 08:36:30 +0530

    enter_detail_ui_change

--- lib/ui_designs/enter_detail.dart
+++ lib/ui_designs/enter_detail.dart
@@ -43,207 +43,198 @@ class _EnterDetailState extends State<EnterDetail> {
       appBar: AppBar(
         title: Text('Dog Detail'),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.blue[400],
-        ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 15, right: 15),
-            child: Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 10,
-                      color: Colors.blueAccent,
+      body: ListView(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.only(top: 20, right: 10, left: 10, bottom: 10),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 10,
+                    color: Colors.blueAccent,
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 25,
+                      bottom: 10,
                     ),
-                  ],
-                ),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 25,
-                          bottom: 10,
+                    child: Text(
+                      'ADD DETAIL OF DOG',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Times New Roman',
+                        shadows: [
+                          Shadow(
+                            blurRadius: 1,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        SizedBox(
+                          height: 2,
                         ),
-                        child: Text(
-                          'ADD DETAIL OF DOG',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Times New Roman',
-                            shadows: [
-                              Shadow(
-                                blurRadius: 1,
-                              ),
-                            ],
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: _nameController,
+                            textInputAction: TextInputAction.next,
+                            validator: (s) {
+                              if (s!.isEmpty) {
+                                return 'Enter the name of dog First';
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Dog Name',
+                              hintText: 'Enter Name Of Dog',
+                              icon: Icon(Icons.pets_rounded),
+                            ),
                           ),
                         ),
-                      ),
-                      Form(
-                        key: _formKey,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            SizedBox(
-                              height: 2,
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: _colorController,
+                            textInputAction: TextInputAction.next,
+                            validator: (s) {
+                              if (s!.isEmpty) {
+                                return 'Enter the dog color First';
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Dog Color',
+                              hintText: 'Enter Color Of Dog',
+                              icon: Icon(Icons.color_lens),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: TextFormField(
-                                controller: _nameController,
-                                textInputAction: TextInputAction.next,
-                                validator: (s) {
-                                  if (s!.isEmpty) {
-                                    return 'Enter the name of dog First';
-                                  }
-                                  return null;
-                                },
-                                decoration: InputDecoration(
-                                  labelText: 'Dog Name',
-                                  hintText: 'Enter Name Of Dog',
-                                  icon: Icon(Icons.pets_rounded),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: TextFormField(
-                                controller: _colorController,
-                                textInputAction: TextInputAction.next,
-                                validator: (s) {
-                                  if (s!.isEmpty) {
-                                    return 'Enter the dog color First';
-                                  }
-                                  return null;
-                                },
-                                decoration: InputDecoration(
-                                  labelText: 'Dog Color',
-                                  hintText: 'Enter Color Of Dog',
-                                  icon: Icon(Icons.color_lens),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: TextFormField(
-                                controller: _amountController,
-                                textInputAction: TextInputAction.next,
-                                validator: (s) {
-                                  if (s!.isEmpty) {
-                                    return 'Enter the age of dog First';
-                                  }
-                                  return null;
-                                },
-                                keyboardType: TextInputType.number,
-                                decoration: InputDecoration(
-                                  labelText: 'Dog Age',
-                                  hintText: 'Enter the Age of Dog',
-                                  icon: Icon(Icons.add_reaction),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: TextFormField(
-                                controller: _descriptionController,
-                                textInputAction: TextInputAction.next,
-                                validator: (des) {
-                                  if (des!.isEmpty) {
-                                    return 'Enter the breed of dog First';
-                                  }
-                                  return null;
-                                },
-                                decoration: InputDecoration(
-                                  labelText: 'Dog Breed',
-                                  hintText: 'Enter Breed of Dog',
-                                  icon: Icon(Icons.whatshot_rounded),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: TextFormField(
-                                controller: _dateController,
-                                textInputAction: TextInputAction.next,
-                                validator: (des) {
-                                  if (des!.isEmpty) {
-                                    return 'Enter extra description please';
-                                  }
-                                  return null;
-                                },
-                                decoration: InputDecoration(
-                                  labelText: 'Description',
-                                  hintText: 'Enter extra info of Dog',
-                                  icon: Icon(Icons.description),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: MaterialButton(
-                                color: Colors.blue,
-                                child: Text(
-                                  widget.buttonName != null
-                                      ? widget.buttonName!
-                                      : 'Save',
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 19,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                onPressed: () {
-                                  final FormState? formm =
-                                      _formKey.currentState as FormState?;
-                                  if (formm!.validate()) {
-                                    print(
-                                        'Expense is ${_amountController.text.toString()}');
-                                    final newexpensess = DogModel(
-                                      dogAge: _amountController.text.toString(),
-                                      dogBreed: _descriptionController.text
-                                          .toString(),
-                                      dogName: _nameController.text.toString(),
-                                      date: _dateController.text.toString(),
-                                      dogColor:
-                                          _colorController.text.toString(),
-                                    );
-
-                                    if (_amountController.text
-                                        .toString()
-                                        .isNotEmpty) {
-                                      var catId = _databaseProvider
-                                          .addTransaction(newexpensess);
-                                      print('${catId.toString()}');
-                                    }
-                                    if (widget.newexpenseModel != null) {
-                                      newexpensess.id =
-                                          widget.newexpenseModel!.id;
-                                      _databaseProvider
-                                          .updateTransaction(newexpensess);
-                                    }
-                                    Navigator.pop(context);
-                                  }
-                                },
-                              ),
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: _amountController,
+                            textInputAction: TextInputAction.next,
+                            validator: (s) {
+                              if (s!.isEmpty) {
+                                return 'Enter the age of dog First';
+                              }
+                              return null;
+                            },
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              labelText: 'Dog Age',
+                              hintText: 'Enter the Age of Dog',
+                              icon: Icon(Icons.add_reaction),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: _descriptionController,
+                            textInputAction: TextInputAction.next,
+                            validator: (des) {
+                              if (des!.isEmpty) {
+                                return 'Enter the breed of dog First';
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Dog Breed',
+                              hintText: 'Enter Breed of Dog',
+                              icon: Icon(Icons.whatshot_rounded),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: _dateController,
+                            textInputAction: TextInputAction.next,
+                            validator: (des) {
+                              if (des!.isEmpty) {
+                                return 'Enter extra description please';
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Description',
+                              hintText: 'Enter extra info of Dog',
+                              icon: Icon(Icons.description),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: MaterialButton(
+                            color: Colors.blue,
+                            child: Text(
+                              widget.buttonName != null
+                                  ? widget.buttonName!
+                                  : 'Save',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            onPressed: () {
+                              final FormState? formm =
+                                  _formKey.currentState as FormState?;
+                              if (formm!.validate()) {
+                                print(
+                                    'Expense is ${_amountController.text.toString()}');
+                                final newexpensess = DogModel(
+                                  dogAge: _amountController.text.toString(),
+                                  dogBreed:
+                                      _descriptionController.text.toString(),
+                                  dogName: _nameController.text.toString(),
+                                  date: _dateController.text.toString(),
+                                  dogColor: _colorController.text.toString(),
+                                );
+
+                                if (_amountController.text
+                                    .toString()
+                                    .isNotEmpty) {
+                                  var catId = _databaseProvider
+                                      .addTransaction(newexpensess);
+                                  print('${catId.toString()}');
+                                }
+                                if (widget.newexpenseModel != null) {
+                                  newexpensess.id = widget.newexpenseModel!.id;
+                                  _databaseProvider
+                                      .updateTransaction(newexpensess);
+                                }
+                                Navigator.pop(context);
+                              }
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
+                ],
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
