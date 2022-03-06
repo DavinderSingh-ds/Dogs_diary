999dd0d70fb73c65521d50fcddfc0338727dc763
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-03-06 12:12:47 +0530

    Localization_fixing

--- lib/Screens/Chatting.dart
+++ lib/Screens/Chatting.dart
@@ -1,32 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import '../Widgets/Language_picker_widget.dart';
+import 'package:dog_app/Widgets/Language_picker_widget.dart';
 
 class MessageBox extends StatefulWidget {
   @override
-  _MessageBoxState createState() => _MessageBoxState();
+  State<MessageBox> createState() => _MessageBoxState();
 }
 
 class _MessageBoxState extends State<MessageBox> {
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              LanguageWidget(),
-              const SizedBox(height: 32),
-              Text(
-                AppLocalizations.of(context)!.language,
-                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                AppLocalizations.of(context)!.hello('Davinder Singh'),
-                style: TextStyle(fontSize: 36),
-              ),
-            ],
-          ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            LanguageWidget(),
+            const SizedBox(height: 32),
+            Text(
+              AppLocalizations.of(context).toString(),
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 50),
+            MaterialButton(
+              color: Colors.amber,
+              minWidth: 200,
+              height: 50,
+              onPressed: () {},
+              child: Text('Click'),
+            ),
+          ],
         ),
-      );
+      ),
+    );
+  }
 }
